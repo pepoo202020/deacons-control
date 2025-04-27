4c1d03bce8285f629858539cbe0784aac327d69a
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2025-04-27 15:47:37 +0300

    CREATED: menu nav struture and ui and constants

--- constants/dashboard.data.ts
+++ constants/dashboard.data.ts
@@ -1,6 +1,20 @@
+import { FaHome } from "react-icons/fa"
+
 export const HEADER_DATA = {
     mainTitle: {
         EN: "Dashboard For",
         AR: "لوحة التحكم ال"
     },
 }
+
+export const MENU_NAV_DATA = [
+    {
+        title: {
+            EN: "Home",
+            AR: "الرئيسية"
+        },
+        icon: FaHome,
+        link: '/dashboard',
+        roles: ['admin', 'controller', 'teacher']
+    }
+]
--- ui/dashboard/menu-nav.tsx
+++ ui/dashboard/menu-nav.tsx
@@ -1,7 +1,34 @@
+'use client'
+import { MENU_NAV_DATA } from '@/constants/dashboard.data'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function MenuNav() {
+  const lang = 'AR'
+  const pathname = usePathname()
   return (
-    <div>MenuNav</div>
+    <div
+      className='w-full px-10 flex items-center justify-center bg-neutral-300 dark:bg-blue-950 text-blue-950 dark:text-white space-x-1 overflow-y-auto'
+    >
+      {
+        MENU_NAV_DATA.map((menu_item, index) => (
+          <Link href={menu_item.link} key={index} className={
+            cn(
+              pathname === menu_item.link && "bg-blue-950 text-white dark:bg-white dark:text-blue-950 cursor-auto" ,
+              "h-full w-fit px-1 py-3",
+              "hover:bg-blue-950 hover:text-white hover:dark:bg-white hover:dark:text-blue-950",
+              "transition-all duration-200"
+            )
+          }>
+            <span className='flex flex-col items-center justify-center'>
+              <menu_item.icon size={20} />
+            </span>
+            <span>{menu_item.title[lang]}</span>
+          </Link>
+        ))
+      }
+    </div>
   )
 }
